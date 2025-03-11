a8695f58542b0d62de5908476930670c4f4d1179
BelisnalvaCosta <bella-costa@hotmail.com.br>
2025-03-11 14:47:16 -0300

    Committing all folders, packages and components, pages

---
+++ wiki/src/pages/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    heigth: 90vh;
+
+    display: flex;
+    alignItems: flex-start;
+    margin: 20px 0 0 0;
+    justify-content: center;
+`
